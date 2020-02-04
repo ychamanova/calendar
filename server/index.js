e39f13a448885a6e5c9f3d7e991953748c5896ee
Yelena Chamanova <ychamanova@gmail.com>
2020-02-04 09:08:47 -0800

    implement some style changes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3004;
-const db = require('../database/schema.js')
+const db = require('../database/schema.js');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,27 +12,33 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/id/reservation', (req, res) => {
-    let reservation = (req.query);
-    const partySize = reservation.size; //requested party size
-    db.Restaurant.find({
-        restaurantId: reservation.id //find the restaurant in the database based on id
-    }, (err, results) => {
-        const timeslots = results[0].timeslots; //array of all reservations for this restaurant
-        const maxHeadCount = results[0].maxHeadCount; //how many people this restaurant can handle
+  const reservation = (req.query);
+  // requested party size
+  const partySize = reservation.size;
+  db.Restaurant.find({
+    // find the restaurant in the database based on id
+    restaurantId: reservation.id,
+  }, (err, results) => {
+    // array of all reservations for this restaurant
+    const { timeslots } = results[0];
+    // how many people this restaurant can handle
+    const { maxHeadCount } = results[0];
+    // returns an array of reservations under that time. it will always be just one object in this array
+    const thisTimeslot = timeslots.filter((timeSlot) => timeSlot.time === reservation.time);
 
-        const thisTimeslot = timeslots.filter((timeSlot) => timeSlot.time === reservation.time);//returns an array of reservations under that time. it will always be just one object in this array
+    // if the reservations in this timeslot exist (are bigger than 0 reservations)
+    // and party size is smaller than maximum allowed spots minus already taken spots or if the reservations in this timeslot don't exist and max allowed headcount doesn't exceed the partysize
+    // send available timeslots
+    // otherwise send not available
 
-        //if the reservations in this timeslot exist (are bigger than 0 reservations)
-        //and party size is smaller than maximum allowed spots minus already taken spots or if the reservations in this timeslot don't exist and max allowed headcount doesn't exceed the partysize
-        //send available
-        //otherwise send not available
-
-        ((thisTimeslot.length > 0) && (partySize < (maxHeadCount - thisTimeslot[0].currentHeadCount)) ||
-            ((thisTimeslot.length === 0) && (partySize < maxHeadCount)))
-            ? res.end(
-                JSON.stringify(['7:15 PM', '7:30 PM', '8:00 PM']) //send three available dates to book the place
-            )
-            : res.end('not available')
-    })
-})
-app.listen(port, () => console.log(`Open Table reservations listening on port ${port}!`));
+    if ((thisTimeslot.length > 0)
+      && (partySize < (maxHeadCount - thisTimeslot[0].currentHeadCount))
+      || ((thisTimeslot.length === 0) && (partySize < maxHeadCount))) {
+      // send three available dates to book the place
+      res.end(JSON.stringify(['7:15 PM', '7:30 PM', '8:00 PM']));
+    } else {
+      res.end('not available');
+    }
+  });
+});
+app.listen(port);
--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import DatePicker from "react-datepicker";
-import moment from "moment";
-import "../styles/Datepicker.css";
-import Styles from "../styles/Calendar.css";
-import axios from "axios";
-import AvailableTimeButton from "./AvailableTimeButton.jsx";
+import React from 'react';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import axios from 'axios';
+import AvailableTimeButton from './AvailableTimeButton.jsx';
+import '../styles/Datepicker.css';
+import Styles from '../styles/Calendar.css';
+
 
 class Calendar extends React.Component {
   constructor(props) {
@@ -13,7 +14,7 @@ class Calendar extends React.Component {
       partySize: 1,
       date: moment(new Date()),
       availableTimes: [],
-      noTimes: false
+      noTimes: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -31,26 +32,26 @@ class Calendar extends React.Component {
     this.setState({
       [event.target.name]: event.target.value,
       noTimes: false,
-      availableTimes: []
+      availableTimes: [],
     });
   }
 
   handleChangeDate(date) {
-    this.setState({ date: date });
+    this.setState({ date });
   }
 
   handleSubmit(event) {
     event.preventDefault();
     axios
-      .get("id/reservation", {
+      .get('id/reservation', {
         params: {
           id: 1,
           size: this.state.partySize,
           time: this.state.time
         }
       })
-      .then(results => {
-        if (results.data !== "not available") {
+      .then((results) => {
+        if (results.data !== 'not available') {
           this.setState({ noTimes: false });
           this.setState({ availableTimes: results.data });
         } else {
@@ -63,7 +64,7 @@ class Calendar extends React.Component {
   }
 
   reserve() {
-    alert("reserved");
+    alert('reserved');
   }
 
   render() {
@@ -75,30 +76,30 @@ class Calendar extends React.Component {
             Party size:
             <select
               className={Styles.partySize}
-              name="partySize"
+              name='partySize'
               value={this.state.partySize}
               onChange={this.handleChange}
             >
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-              <option value="11">11</option>
-              <option value="12">12</option>
-              <option value="13">13</option>
-              <option value="14">14</option>
-              <option value="15">15</option>
-              <option value="16">16</option>
-              <option value="17">17</option>
-              <option value="18">18</option>
-              <option value="19">19</option>
-              <option value="20">20</option>
+              <option value='1'>1</option>
+              <option value='2'>2</option>
+              <option value='3'>3</option>
+              <option value='4'>4</option>
+              <option value='5'>5</option>
+              <option value='6'>6</option>
+              <option value='7'>7</option>
+              <option value='8'>8</option>
+              <option value='9'>9</option>
+              <option value='10'>10</option>
+              <option value='11'>11</option>
+              <option value='12'>12</option>
+              <option value='13'>13</option>
+              <option value='14'>14</option>
+              <option value='15'>15</option>
+              <option value='16'>16</option>
+              <option value='17'>17</option>
+              <option value='18'>18</option>
+              <option value='19'>19</option>
+              <option value='20'>20</option>
             </select>
           </label>
 
@@ -118,40 +119,40 @@ class Calendar extends React.Component {
               <label>
                 Time:
                 <select
-                  name="time"
+                  name='time'
                   value={this.state.time}
                   onChange={this.handleChange}
                 >
-                  <option value="9:00 AM">9:00 AM</option>
-                  <option value="9:30 AM">9:30 AM</option>
-                  <option value="10:00 AM">10:00 AM</option>
-                  <option value="10:30 AM">10:30 AM</option>
-                  <option value="11:00 AM">11:00 AM</option>
-                  <option value="11:30 AM">11:30 AM</option>
-                  <option value="12:00 AM">12:00 PM</option>
-                  <option value="12:30 AM">12:30 PM</option>
-                  <option value="1:00 PM">1:00 PM</option>
-                  <option value="1:30 PM">1:30 PM</option>
-                  <option value="2:00 PM">2:00 PM</option>
-                  <option value="2:30 PM">2:30 PM</option>
-                  <option value="3:00 PM">3:00 PM</option>
-                  <option value="3:30 PM">3:30 PM</option>
-                  <option value="4:00 PM">4:00 PM</option>
-                  <option value="4:30 PM">4:30 PM</option>
-                  <option value="5:00 PM">5:00 PM</option>
-                  <option value="5:30 PM">5:30 PM</option>
-                  <option value="6:00 PM">6:00 PM</option>
-                  <option value="6:30 PM">6:30 PM</option>
-                  <option value="7:00 PM">7:00 PM</option>
-                  <option value="7:30 PM">7:30 PM</option>
-                  <option value="8:00 PM">8:00 PM</option>
-                  <option value="8:30 PM">8:30 PM</option>
-                  <option value="9:00 PM">9:00 PM</option>
-                  <option value="9:30 PM">9:30 PM</option>
-                  <option value="10:00 PM">10:00 PM</option>
-                  <option value="10:30 PM">10:30 PM</option>
-                  <option value="11:00 PM">11:00 PM</option>
-                  <option value="11:30 PM">11:30 PM</option>
+                  <option value='9:00 AM'>9:00 AM</option>
+                  <option value='9:30 AM'>9:30 AM</option>
+                  <option value='10:00 AM'>10:00 AM</option>
+                  <option value='10:30 AM'>10:30 AM</option>
+                  <option value='11:00 AM'>11:00 AM</option>
+                  <option value='11:30 AM'>11:30 AM</option>
+                  <option value='12:00 AM'>12:00 PM</option>
+                  <option value='12:30 AM'>12:30 PM</option>
+                  <option value='1:00 PM'>1:00 PM</option>
+                  <option value='1:30 PM'>1:30 PM</option>
+                  <option value='2:00 PM'>2:00 PM</option>
+                  <option value='2:30 PM'>2:30 PM</option>
+                  <option value='3:00 PM'>3:00 PM</option>
+                  <option value='3:30 PM'>3:30 PM</option>
+                  <option value='4:00 PM'>4:00 PM</option>
+                  <option value='4:30 PM'>4:30 PM</option>
+                  <option value='5:00 PM'>5:00 PM</option>
+                  <option value='5:30 PM'>5:30 PM</option>
+                  <option value='6:00 PM'>6:00 PM</option>
+                  <option value='6:30 PM'>6:30 PM</option>
+                  <option value='7:00 PM'>7:00 PM</option>
+                  <option value='7:30 PM'>7:30 PM</option>
+                  <option value='8:00 PM'>8:00 PM</option>
+                  <option value='8:30 PM'>8:30 PM</option>
+                  <option value='9:00 PM'>9:00 PM</option>
+                  <option value='9:30 PM'>9:30 PM</option>
+                  <option value='10:00 PM'>10:00 PM</option>
+                  <option value='10:30 PM'>10:30 PM</option>
+                  <option value='11:00 PM'>11:00 PM</option>
+                  <option value='11:30 PM'>11:30 PM</option>
                 </select>
               </label>
             </div>
@@ -159,7 +160,7 @@ class Calendar extends React.Component {
           <br />
 
           {this.state.availableTimes.length === 0 && !this.state.noTimes && (
-            <input className={Styles.btn} type="submit" value="Find a Table" />
+            <input className={Styles.btn} type='submit' value='Find a Table' />
           )}
 
           {this.state.noTimes && (
